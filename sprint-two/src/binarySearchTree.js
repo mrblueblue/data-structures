02e50e7567054c85fe0dd6bcbe8e1f438bb119f6
mrblueblue <grokut@gmail.com>
2015-02-06 18:49:52 -0800

    completed bi-tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,71 @@
 var BinarySearchTree = function(value){
+
+  var binaryTree = {};
+
+  binaryTree.left = null;
+  binaryTree.right = null;
+  binaryTree.value = value;
+
+  _.extend(binaryTree, binaryTreeMethods);
+  return binaryTree;
+
 };
 
+var binaryTreeMethods ={};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+binaryTreeMethods.insert = function(value) {
+
+  if (value < this.value){
+
+    if (this.left === null) {
+      this.left =  BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+
+  } else {
+
+    if (this.right === null) {
+      this.right =  BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+
+  }
+
+};
+
+binaryTreeMethods.contains = function(value) {
+
+  if (this.value === value){
+    return true;
+  }
+
+  if (this.value < value){
+
+    if (this.right !== null){
+      return this.right.contains(value);
+    }else{
+      return false;
+    }
+  }
+  if(this.left !== null){
+    return this.left.contains(value);
+  }else{
+    return false;
+  }
+};
+
+binaryTreeMethods.depthFirstLog = function(callback) {
+
+  callback(this.value);
+
+  if (this.left !== null){
+    this.left.depthFirstLog(callback);
+  }
+
+  if (this.right !== null){
+    this.right.depthFirstLog(callback);
+  }
+  // body...
+};
